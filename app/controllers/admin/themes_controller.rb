22e30b13a29841fa76382949822dcece30f14494
daichi <daichi@suzukidaichinoMacBook-Pro-2.local>
2020-02-09 14:55:19 +0900

    管理者新規テーマ投稿機能実装

--- app/controllers/admin/themes_controller.rb
+++ app/controllers/admin/themes_controller.rb
@@ -5,11 +5,16 @@ class Admin::ThemesController < ApplicationController
     end
 
     def new
-
+        @theme = Theme.new
     end
 
     def create
-
+        @theme = Theme.new(theme_params)
+        if @theme.save
+            redirect_to admin_themes_path
+        else
+            render :new
+        end
     end
 
     def edit
@@ -23,5 +28,10 @@ class Admin::ThemesController < ApplicationController
     def destroy
 
     end
+
+    private
+        def theme_params
+            perams.requre(:theme).permit(:name, :introduction, :is_active, :image)
+        end
     
 end
