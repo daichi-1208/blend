e653a5a24094a00d78394094710e884ae692fa58
daichi <daichi@suzukidaichinoMacBook-Pro-2.local>
2020-02-19 21:32:20 +0900

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,9 @@ class HomeController < ApplicationController
         @new_posts = Post.all.order(created_at: :desc).limit(6)
         #カクテルランキング表示
         @post_ranks = Post.create_all_ranks
+
+
+        #ランダムでお題を3件表示
+        @random = Theme.order("RANDOM()").limit(1)
     end
 end
