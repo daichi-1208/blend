bc2c8750e25eca20e7762fe0d8c129b2325c00bd
daichi <daichi@suzukidaichinoMacBook-Pro-2.local>
2020-02-22 20:02:19 +0900

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,12 @@ class HomeController < ApplicationController
 
     def top
         #新着カクテル表示
-        @new_posts = Post.all.order(created_at: :desc).limit(6)
+        @new_posts = Post.all.order(created_at: :desc).limit(3)
         #カクテルランキング表示
         @post_ranks = Post.create_all_ranks
-
-
-        #ランダムでお題を3件表示
+        #ユーザーランキング表示
+        @user_ranks = User.create_all_ranks
+        #ランダムでお題を1件表示
         @random = Theme.order("RANDOM()").limit(1)
     end
 end
