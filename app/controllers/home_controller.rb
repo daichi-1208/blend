90be09a0aa86cff5b0ef8f746f0409a7d85f5c76
daichi <daichi@suzukidaichinoMacBook-Pro-2.local>
2020-02-25 21:08:03 +0900

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
         @new_posts = Post.all.order(created_at: :desc).limit(3)
         @post_ranks = Post.create_all_ranks
         @user_ranks = User.create_all_ranks
-        @random = Theme.order("RANDOM()").limit(1)
+        @random = Theme.order("RAND()").limit(1)
+        # @random = Theme.order("RANDOM()").limit(1)
     end
 end
